ec97d3786d1edfb29455c1294260e3c98ae76a30
simontaosim <xsqfeather@126.com>
2015-01-26 07:20:53 +0000

    123

---
+++ models/node_way.rb
@@ -0,0 +1,17 @@
+class NodeWay
+  include Mongoid::Document
+  include Mongoid::Timestamps # adds created_at and updated_at fields
+
+  # field <name>, :type => <type>, :default => <value>
+  field :node1, :type => String
+  field :node2, :type => String
+  field :fee, :type => Float
+  field :speed, :type => Integer
+  field :miles, :type => String
+
+  # You can define indexes on documents using the index macro:
+  # index :field <, :unique => true>
+
+  # You can create a composite key in mongoid to replace the default id using the key macro:
+  # key :field <, :another_field, :one_more ....>
+end
